42774253cfc1c2823bb178e5abb076a3ac351ec5
Mehdi CHEBBAH <gm_chebbah@esi.dz>
2022-01-22 16:04:27 +0100

    finished the answer and results endpoints

--- routes/surveys.js
+++ routes/surveys.js
@@ -14,6 +14,32 @@ const router = express.Router();
 var getSharedURL = (surveyID)=>{
     return `/api/v0/surveys/${surveyID}`;
 }
+
+var readSurvey = (surveyID)=>{
+    return require(`../Surveys/${surveyID}.json`)
+}
+
+var writeSurvey = (surveyID, survey)=>{
+    fs.writeFile(`Surveys/${surveyID}.json`, JSON.stringify(survey), 'utf8', (err)=>{
+        if(err) throw {error: "Error saving your survey, Please try again!"};
+    });
+}
+
+var verifyAnswers = (questions, answers) => {
+    let nbrSections = Object.keys(questions).length
+    console.log("out:", nbrSections , Object.keys(answers).length - 1)
+    if(nbrSections != Object.keys(answers).length - 1){
+        return false;
+    }else{
+        for(let i=0; i<nbrSections; i++){
+            console.log("in:", nbrSections , Object.keys(answers).length - 1)
+            if(Object.keys(questions[i]).length -1 != Object.keys(answers[i]).length){ // The -1 is for the title of the section field which doesn't exist in the answers
+                return false;
+            }
+        }
+    }
+    return true;
+}
 /************* Routes ************* */
     /**
      * Create a Survey
@@ -25,23 +51,22 @@ var getSharedURL = (surveyID)=>{
                 ID: id,
                 title: req.body.title || "Not Specified",
                 description: req.body.description || "Not Specified",
+                createdAt: new Date().toISOString(),
                 questions: {},
-                answers: []
+                answers: {}
             };
             req.body.questions.forEach( (element, index) => {
                 req.body.questions[index] = {sectionTitle: element.title, ...element.questions}
             });
             survey.questions = {...req.body.questions}
 
-            fs.writeFile(`Surveys/${id}.json`, JSON.stringify(survey), 'utf8', (err)=>{
-                if(err) throw err;
-            });
+            writeSurvey(id, survey);
 
             res.status(200);
-            res.send(survey)
+            res.send({sharedURL: getSharedURL(id), ...survey})
         }catch(err){
             res.status(500);
-            res.send({error: "Error saving your survey, Please try again!"});
+            res.send(err);
         }
     });
 
@@ -50,30 +75,48 @@ var getSharedURL = (surveyID)=>{
      */
     router.get('/:id', async (req, res) => {
 
-        let id = req.params.id;
         try{
-            var {ID, title, description, questions} = require(`../Surveys/${id}.json`)
+            let id = req.params.id;
+            let {ID, title, description, questions, createdAt} = readSurvey(id);
+            res.status(200);
+            res.send({ID, title, description, questions, createdAt, sharedURL: getSharedURL(id)})
         }catch(err){
             res.status(404).send({error: "Survey Not Found"});
         }
 
-        res.status(200);
-        res.send({ID, title, description, questions})
+        
     });
 
     /**
      * Answer a Survey
      */
-     router.put('/', async (req, res) => {
+     router.put('/:id', async (req, res) => {
         try{
+
+            let id = req.params.id;
+
+            let new_answer = req.body;
+            new_answer.createdAt = new Date().toISOString();
+            let survey = {}
+            try{
+                survey = readSurvey(id)
+            }catch(err){
+                res.status(404).send({error: "Survey Not Found."});
+            }
+
+            if(!verifyAnswers(survey.questions, new_answer)){
+                throw {error: "You didn't answer correctly the questions"};
+            }
+
+            index = Object.keys(survey["answers"]).length.toString();
+            survey["answers"][index] = new_answer;
     
+            writeSurvey(id, survey)
             res.status(200);
-            res.send({
-                // Some data here
-            })
+            res.send(survey)
         }catch(err){
             res.status(500);
-            res.send({error: err});
+            res.send(err || {error: "something went wrong, Please try again!!"});
         }
     });
     
@@ -82,11 +125,17 @@ var getSharedURL = (surveyID)=>{
      */
      router.get('/:id/results', async (req, res) => {
         try{
-    
+            let id = req.params.id;
+
+            let survey = {}
+            try{
+                survey = readSurvey(id)
+            }catch(err){
+                res.status(404).send({error: "Survey Not Found."});
+            }
+
             res.status(200);
-            res.send({
-                // Some data here
-            })
+            res.send(survey)
         }catch(err){
             res.status(500);
             res.send({error: err});
