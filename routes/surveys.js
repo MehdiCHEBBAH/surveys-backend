6a717125a0076e959316dd231afbc35cc2a58908
Mehdi CHEBBAH <gm_chebbah@esi.dz>
2022-01-22 11:49:37 +0100

    finished create and get routes

--- routes/surveys.js
+++ routes/surveys.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { randomUUID } = require('crypto');
+var fs = require('fs');
+
 var config = require("../config.json");
 
 const router = express.Router();
@@ -6,29 +9,62 @@ const router = express.Router();
 
 /************** Global Vars ************/
 
+
+/************** Tools *****************/
+var getSharedURL = (surveyID)=>{
+    return `/api/v0/surveys/${surveyID}`;
+}
 /************* Routes ************* */
     /**
      * Create a Survey
      */
     router.post('/', async (req, res) => {
         try{
-    
+            id = randomUUID();
+            let survey = {
+                ID: id,
+                title: req.body.title || "Not Specified",
+                description: req.body.description || "Not Specified",
+                questions: {},
+                answers: []
+            };
+            req.body.questions.forEach( (element, index) => {
+                req.body.questions[index] = {sectionTitle: element.title, ...element.questions}
+            });
+            survey.questions = {...req.body.questions}
+
+            fs.writeFile(`Surveys/${id}.json`, JSON.stringify(survey), 'utf8', (err)=>{
+                if(err) throw err;
+            });
+
             res.status(200);
-            res.send({
-                // Some data here
-            })
+            res.send(survey)
         }catch(err){
             res.status(500);
-            res.send({error: err});
+            res.send({error: "Error saving your survey, Please try again!"});
         }
     });
 
     /**
      * Get a Survey
-     * Note: 
-     *    There are two cases here: The surey creator, and a survey taker 
      */
-     router.get('/', async (req, res) => {
+    router.get('/:id', async (req, res) => {
+
+        let id = req.params.id;
+        try{
+            var {ID, title, description, questions} = require(`../Surveys/${id}.json`)
+        }catch(err){
+            res.status(404).send({error: "Survey Not Found"});
+        }
+
+        res.status(200);
+        res.send({ID, title, description, questions})
+    });
+
+    /**
+     * Answer a Survey
+     */
+     router.put('/', async (req, res) => {
         try{
     
             res.status(200);
@@ -40,11 +76,11 @@ const router = express.Router();
             res.send({error: err});
         }
     });
-
+    
     /**
-     * Answer a Survey
+     * Get the results of a Survey
      */
-     router.put('/', async (req, res) => {
+     router.get('/:id/results', async (req, res) => {
         try{
     
             res.status(200);
